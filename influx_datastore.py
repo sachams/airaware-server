8c82f76d9fc3af9560b51f20f87e8b42ab1280f6
Sacha Manson-Smith <smansonsmith@gmail.com>
2023-09-20 18:19:08 +0100

    Combine read functions and add downsampling

--- influx_datastore.py
+++ influx_datastore.py
@@ -74,33 +74,7 @@ class InfluxDatastore:
         # so add this now
         return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    def read_data(self, site_code, series, start, end):
-        """Reads data from the datastore, and returns it in this format:
-        [
-            {
-                'time': '2023-06-12T10:00:00',
-                'value': 12.983285921708745
-            }
-        ]
-        """
-        query = (
-            "select time, value "
-            f'from "{series}" '
-            f"where site_code = '{site_code}' "
-            f"and time >= '{InfluxDatastore._isoformat_utc(start)}' "
-            f"and time < '{InfluxDatastore._isoformat_utc(end)}' "
-            "order by time asc"
-        )
-
-        try:
-            results = self.client.query(query=query, language="sql", mode="pandas")
-        except pa.lib.ArrowInvalid as e:
-            # This indicates the table can't be found
-            return []
-
-        return self.format_results(results)
-
-    def read_average_data(self, series, start, end, site_codes):
+    def read_data(self, series, start, end, site_codes, frequency):
         """Reads data from the datastore, averaging across the specified sites. If no
         sites are specified, it averages across all sites.
         and returns it in this format:
@@ -110,6 +84,10 @@ class InfluxDatastore:
                 'value': 12.983285921708745
             }
         ]
+
+        site_codes: a list of sites to average over. If empty, it averages over all
+                    sites.
+        frequency:  either `hourly` or `daily`
         """
         site_clause = ""
 
@@ -119,15 +97,33 @@ class InfluxDatastore:
             )
             site_clause = f"and site_code in ({formatted_site_codes})"
 
-        query = (
-            "select time, avg(value) as 'value' "
-            f'from "{series}" '
-            f"where time >= '{InfluxDatastore._isoformat_utc(start)}' "
-            f"and time < '{InfluxDatastore._isoformat_utc(end)}' "
-            f"{site_clause} "
-            "group by time "
-            "order by time asc"
-        )
+        match (frequency):
+            case "hourly":
+                query = (
+                    "select time, avg(value) as 'value' "
+                    f'from "{series}" '
+                    f"where time >= '{InfluxDatastore._isoformat_utc(start)}' "
+                    f"and time < '{InfluxDatastore._isoformat_utc(end)}' "
+                    f"{site_clause} "
+                    "group by time "
+                    "order by time asc"
+                )
+
+            case "daily":
+                query = (
+                    "select _time as time, value from ( "
+                    f"select date_bin(interval '1 day', time) as _time, avg(value) as 'value' "
+                    f'from "{series}" '
+                    f"where time >= '{InfluxDatastore._isoformat_utc(start)}' "
+                    f"and time < '{InfluxDatastore._isoformat_utc(end)}' "
+                    f"{site_clause} "
+                    "group by _time "
+                    "order by _time asc"
+                    ")"
+                )
+
+            case _:
+                raise Exception(f"Unknown frequency {frequency}")
 
         try:
             results = self.client.query(query=query, language="sql", mode="pandas")
--- sync_data.py
+++ sync_data.py
@@ -54,36 +54,20 @@ def sync(site_code, series, resync):
 
 
 @cli.command()
-@click.argument("site_code", required=True)
 @click.argument("series", required=True)
 @click.argument("start", required=True)
 @click.argument("end", required=True)
-def datastore_read(site_code, series, start, end):
-    """Reads data from the datastore"""
-    datastore = InfluxDatastore(
-        app_config.influx_host,
-        app_config.influx_token,
-        app_config.influx_org,
-        app_config.influx_database,
-    )
-
-    start_date = datetime.datetime.fromisoformat(start)
-    end_date = datetime.datetime.fromisoformat(end)
-
-    data = datastore.read_data(site_code, series, start_date, end_date)
-
-    for row in data:
-        print(str(row))
-
-
-@cli.command()
-@click.argument("series", required=True)
-@click.argument("start", required=True)
-@click.argument("end", required=True)
+@click.option(
+    "--frequency",
+    "-f",
+    required=False,
+    default="hourly",
+    type=click.Choice(["hourly", "daily"]),
+)
 @click.option("--site_code", "-s", multiple=True, required=False)
-def datastore_read_avg(series, start, end, site_code):
+def datastore_read(series, start, end, site_code, frequency):
     """Reads data from the datastore, averaging across all nodes, or optionally just
-    the specified nodes"""
+    the specified node(s)"""
     datastore = InfluxDatastore(
         app_config.influx_host,
         app_config.influx_token,
@@ -94,7 +78,7 @@ def datastore_read_avg(series, start, end, site_code):
     start_date = datetime.datetime.fromisoformat(start)
     end_date = datetime.datetime.fromisoformat(end)
 
-    data = datastore.read_average_data(series, start_date, end_date, site_code)
+    data = datastore.read_data(series, start_date, end_date, site_code, frequency)
 
     for row in data:
         print(str(row))
