d1fbf390285b2ff5f53f4422274991c226517c78
Sacha Manson-Smith <smansonsmith@gmail.com>
2023-10-13 05:27:10 +0100

    Add broken site filter

--- breathe_london.py
+++ breathe_london.py
@@ -10,13 +10,19 @@ from borough_mapper import BoroughMapper
 
 class BreatheLondon:
     def __init__(self, api_key):
+        # Check if a directory called /data exists. If so, use it as the file root
+        file_root = "/data" if os.path.isdir("/data") else "."
         self.max_retries = 3
         self.retry_pause = 1
         self.client = httpx.Client(timeout=30)
         self.api_key = api_key
         self.base_url = "https://api.breathelondon.org/api"
-        self.site_cache_filename = "/data/site_cache.json"
+
+        self.site_cache_filename = f"{file_root}/site_cache.json"
         self.borough_mapper = BoroughMapper()
+
+        with open(f"broken_sites.json", "r") as broken_sites_file:
+            self.broken_sites = json.load(broken_sites_file)
 
     def _get(self, endpoint):
         """Makes a GET request from the endpoint"""
@@ -43,13 +49,15 @@ class BreatheLondon:
         try:
             if os.path.exists(self.site_cache_filename):
                 logging.info(f"Found site cache at {self.site_cache_filename}")
-                one_day_ago = datetime.datetime.utcnow() + datetime.timedelta(days=-1)
+                max_cache_age = datetime.datetime.utcnow() + datetime.timedelta(
+                    minutes=-10
+                )
                 cache_modified = datetime.datetime.fromtimestamp(
                     os.path.getmtime(self.site_cache_filename)
                 )
                 logging.info(f"Cache last modified {cache_modified}")
 
-                if cache_modified > one_day_ago:
+                if cache_modified > max_cache_age:
                     logging.info("Loading site list from cache")
                     with open(self.site_cache_filename, "r") as cache:
                         site_list = json.load(cache)
@@ -67,6 +75,27 @@ class BreatheLondon:
         except FileNotFoundError:
             logging.warning("Unable to save sites to cache")
 
+    def _fix_status(self, status):
+        match status:
+            case "healthy":
+                return "Healthy"
+
+            case "offline":
+                return "Offline"
+
+            case "coming online":
+                return "Coming online"
+
+            case "needs attention" | "needsAttention":
+                return "Needs attention"
+
+            case _:
+                return "Unknown"
+
+    def _get_status_for_site(self, site_code, current_status):
+        """Amends the site status if the site is on a broken list"""
+        return "Needs attention" if site_code in self.broken_sites else current_status
+
     def get_sites(self):
         """Requests a list of sites from Breathe London and returns list of sites"""
 
@@ -78,10 +107,24 @@ class BreatheLondon:
         logging.info("Loading site list from API")
         site_list = self._get("ListSensors")[0]
 
-        # Add in borough name for each sensor
         for site in site_list:
+            # # Tidy up SiteDescription
+            # if site["SiteDescription"] is None:
+            #     import pdb
+
+            #     pdb.set_trace()
+
+            # site["SiteDescription"] = None if "null" else site["SiteDescription"]
+
+            # Add in borough name for each sensor
             site["Borough"] = self.borough_mapper.get_borough(
                 site["Latitude"], site["Longitude"]
+            )
+
+            # Fix up status - both typos in the status, and also whether the node is on a list
+            # of broken sites
+            site["OverallStatus"] = self._get_status_for_site(
+                site["SiteCode"], self._fix_status(site["OverallStatus"])
             )
 
         self._save_site_cache(site_list)
