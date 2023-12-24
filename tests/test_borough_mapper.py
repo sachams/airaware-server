64e783f9321e9075ea943a14587820d6e7dbe506
Sacha Manson-Smith <smansonsmith@gmail.com>
2023-12-24 10:03:46 +0000

    Fix borough mapper test

--- tests/test_borough_mapper.py
+++ tests/test_borough_mapper.py
@@ -1,12 +1,24 @@
 import pytest
+
 from borough_mapper import BoroughMapper, NotFoundError
 
 
-def test_map_point():
+def test_map_point_ok():
     mapper = BoroughMapper()
 
     borough = mapper.get_borough(51.4624171, -0.1054934)
     assert borough == "Lambeth"
 
+
+def test_map_point_raise():
+    mapper = BoroughMapper(True)
+
     with pytest.raises(NotFoundError):
         mapper.get_borough(1, 1)
+
+
+def test_map_point_no_raise():
+    mapper = BoroughMapper(False)
+
+    borough = mapper.get_borough(1, 1)
+    assert borough == "Outside London"
