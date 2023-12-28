1a1ee060f0eddf24d61e12ea8fab561099bd67b3
Sacha Manson-Smith <smansonsmith@gmail.com>
2023-12-28 11:24:09 +0000

    Add caching to geometry and shrink

--- server/repository/geometry_repository.py
+++ server/repository/geometry_repository.py
@@ -1,14 +1,20 @@
+import functools
 import json
+import logging
 import os
 
 from server.repository.abstract_geometry_repository import AbstractGeometryRepository
 
 
+@functools.cache
 class GeometryRepository(AbstractGeometryRepository):
-    """A repository that returns named geojson geometries"""
+    """A repository that returns named geojson geometries. Note that we need to cache
+    both the function call as well as the class to actually achieve caching"""
 
+    @functools.cache
     def get_geometry(self, name: str) -> str:
         """Returns the named geometry, or None if it doesn't exist"""
+        logging.info(f"Loading geometry {name} from file and caching result")
         path = f"geometry/{name}.json"
 
         if not os.path.exists(path):
