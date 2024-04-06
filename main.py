8f1fb4f08784fc4e03c24089b73e1dbfe33396c2
Sacha Manson-Smith <smansonsmith@gmail.com>
2024-04-06 23:13:46 +0100

    Remove part api_key implementation

--- main.py
+++ main.py
@@ -4,16 +4,7 @@ from http import HTTPStatus
 from typing import Annotated
 
 import redis.asyncio as redis
-from fastapi import (
-    APIRouter,
-    Depends,
-    FastAPI,
-    HTTPException,
-    Query,
-    Request,
-    Response,
-    Security,
-)
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, Query, Request, Response
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import APIKeyHeader
@@ -75,15 +66,6 @@ app.middleware("http")(log_request_middleware)
 app.add_exception_handler(RequestValidationError, request_validation_exception_handler)
 app.add_exception_handler(HTTPException, http_exception_handler)
 app.add_exception_handler(Exception, unhandled_exception_handler)
-
-
-def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
-    if api_key_header in app_config.api_keys:
-        return api_key_header
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid or missing API Key",
-    )
 
 
 def request_key_builder(
